9dcee92c59e8c364452abc82e17bf50a90d4e312
Fang <fbian@cisco.com>
2012-09-19 14:54:20 -0700

    dynamic pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,29 @@ describe "StaticPages" do
       visit '/static_pages/home' 
       page.should have_css("body", text: "Sample App")
     end
+    it "test title on static_pages/home" do
+      visit '/static_pages/home' 
+      page.should have_selector("title", text: "| Home")
+    end
   end
   describe "Test help" do
     it "test staticpages/help" do
       visit '/static_pages/help' 
       page.should have_content("Help")
     end
+    it "test title on static_pages/help" do
+      visit '/static_pages/help' 
+      page.should have_selector("title", text: "| Help")
+    end
   end
   describe "Test about" do
     it "test static_pages/about" do
       visit '/static_pages/about' 
       page.should have_content("About Us")
+    end
+    it "test title on static_pages/about" do
+      visit '/static_pages/about' 
+      page.should have_selector("title", text: "| About")
     end
   end
 end
